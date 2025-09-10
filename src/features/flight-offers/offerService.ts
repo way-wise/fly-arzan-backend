d15bd30018c16ac7e7961dc09e6fcc69d70d8a07
tauhid97k <tauhid.coder@gmail.com>
2025-09-10 22:12:30 +0600

    added pagination to offers list

--- src/features/flight-offers/offerService.ts
+++ src/features/flight-offers/offerService.ts
@@ -20,7 +20,7 @@ export const flightOfferService = {
       destinationLocationCode: queries.destinationLocationCode,
       departureDate: queries.departureDate.toISOString().split("T")[0],
       adults: queries.adults,
-      max: "19",
+      max: "29",
     });
 
     const url = `${FLIGHT_OFFER_API}/shopping/flight-offers?${searchQueries.toString()}`;
