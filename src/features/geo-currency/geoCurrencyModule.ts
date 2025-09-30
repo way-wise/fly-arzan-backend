a5dcb215c43c13c01f80ad2751f3d1fb98ff3367
tauhid97k <tauhid.coder@gmail.com>
2025-09-30 21:38:02 +0600

    add exchange rate api in geo currency for testing

--- src/features/geo-currency/geoCurrencyModule.ts
+++ src/features/geo-currency/geoCurrencyModule.ts
@@ -10,16 +10,29 @@ const app = new Hono();
 app.get("/", async (c) => {
   const forwardedForIp = c.req.header("X-Forwarded-For");
 
+  // Location information from IP Address
   const API_KEY = process.env.GEO_LOCATION_API_KEY;
   const url = `https://api.ipapi.com/api/${forwardedForIp}?access_key=${API_KEY}`;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+  // GET Latest Exchange Rate Based on US Dollar
+  const OPEN_EXCHANGE_API_KEY = process.env.OPEN_EXCHANGE_API_KEY;
+  const exchangeUrl = `https://openexchangerates.org/api/latest.json?app_id=${OPEN_EXCHANGE_API_KEY}`;
 
-    return c.json(data);
+  try {
+    const [geoResponse, exchangeResponse] = await Promise.all([
+      fetch(url),
+      fetch(exchangeUrl),
+    ]);
+
+    const [geoData, exchangeData] = await Promise.all([
+      geoResponse.json(),
+      exchangeResponse.json(),
+    ]);
+
+    return c.json({ geoData, exchangeData });
   } catch (error) {
-    console.error("Error fetching geolocation data:", error);
+    console.error("Error fetching geolocation or exchange rate data:", error);
+    return c.json({ error: "Failed to fetch data" }, 500);
   }
 });
 
